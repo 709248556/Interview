79a92733e08cc23a087ce28db3b28acd9924d082
709248556 <709248556@qq.com>
2019-08-07 09:50:39 +0800

    Integer和int比较

--- src/main/java/August_05/test1.java
+++ src/main/java/August_05/test1.java
@@ -18,5 +18,15 @@ public class test1 {
 //        System.out.println(s == a);   //false
         System.out.println(b == c);
         System.out.println(a.equals(s));
+
+        /**
+         * 虽然==比较的是地址值，但是integer在-128～127的范围内，使用的是常量池的值，因此j 的地址值也是与i 的一样
+         * 
+         * Integer重写了equals方法会先转成int再进行比较
+         */
+        int i=0;
+        Integer j = new Integer(0);
+        System.out.println(i==j);
+        System.out.println(j.equals(i));
     }
 }
