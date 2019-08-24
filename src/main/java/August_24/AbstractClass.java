07ff8a9c3beda16bc5876c004ce41147c9b6a42a
yan <709248556@qq.com>
2019-08-25 00:25:05 +0800

    abstract static synchronized

---
+++ src/main/java/August_24/AbstractClass.java
@@ -0,0 +1,18 @@
+package August_24;
+
+/**
+ * @author yan
+ * @Title: AbstractClass
+ * @Package August_24
+ * @Description:
+ * @date 2019/8/25 0:17
+ * @Version V1.0
+ */
+
+public abstract class AbstractClass {
+    public abstract int add(int a,int b);
+
+    public abstract synchronized int div(int a ,int b);
+
+    public abstract static int asd(int a,int b);
+}
---
+++ src/main/java/August_24/ChildrenClass.java
@@ -0,0 +1,17 @@
+package August_24;
+
+/**
+ * @author yan
+ * @Title: ChildrenClass
+ * @Package August_24
+ * @Description:
+ * @date 2019/8/25 0:23
+ * @Version V1.0
+ */
+
+public class ChildrenClass extends AbstractClass  {
+    @Override
+    public int add(int a, int b) {
+        return 0;
+    }
+}
