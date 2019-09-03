85cb35ab57381b5f6fea09b545faa40dab5357fa
709248556 <709248556@qq.com>
2019-09-03 15:11:15 +0800

    if else

---
+++ src/main/java/September_03/ifElse.java
@@ -0,0 +1,24 @@
+package September_03;
+
+/**
+ * @author yanlianglong
+ * @Title: ifElse.java
+ * @Package September_03
+ * @Description:
+ * @date 2019/9/3 15:07
+ */
+public class ifElse {
+    public static void main(String[] args) {
+        int i = 1;
+        if(i == 0){
+            i =1;
+        }
+        if(i == 1){
+            i=2;
+        }
+        else {
+            i = 3;
+        }
+        System.out.println(i);
+    }
+}
