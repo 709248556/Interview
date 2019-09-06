8d8f93e1fd06ac021db5da5eae60e9092cec8396
709248556 <709248556@qq.com>
2019-09-06 09:46:46 +0800

    byte运算

---
+++ src/main/java/September_06/ByteTest.java
@@ -0,0 +1,20 @@
+package September_06;
+
+/**
+ * @author yanlianglong
+ * @Title: ByteTest.java
+ * @Package September_06
+ * @Description:
+ * @date 2019/9/6 9:45
+ */
+public class ByteTest {
+    public static void main (String[]  args)
+    {
+        Byte var1 = 126;
+        Byte var2 = 127;
+//        Byte var3 = var1 + var2;   //第7行
+    }
+}
+/*
+ * byte类型进行运算时会自动转化为int类型，也就是说计算结果是int，但是却用byte接收,所以编译不通过
+ */
