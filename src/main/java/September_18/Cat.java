d6a763c255c3c053774a7c81372df95ca8b71200
709248556 <709248556@qq.com>
2019-09-18 10:19:21 +0800

    继承抽象类，一定要实现其方法

---
+++ src/main/java/September_18/Cat.java
@@ -0,0 +1,22 @@
+package September_18;
+
+/**
+ * @author yanlianglong
+ * @Title: Cat.java
+ * @Package September_18
+ * @Description:
+ * @date 2019/9/18 10:16
+ */
+abstract class Animal{
+    abstract void say();
+}
+
+public class Cat extends Animal{
+    public Cat(){
+        System.out.printf("I am a cat");
+    }
+    public static void main(String[] args) {
+        Cat cat=new Cat();
+    }
+    
+}
