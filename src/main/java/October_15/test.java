4f1470f2f1098d21c5d2a9e28c5bcd0e1ebc5e1c
709248556 <709248556@qq.com>
2019-10-15 09:59:29 +0800

    溢出

---
+++ src/main/java/October_15/test.java
@@ -0,0 +1,17 @@
+package October_15;
+
+/**
+ * @author yanlianglong
+ * @Title: test.java
+ * @Package October_15
+ * @Description:
+ * @date 2019/10/15 9:58
+ */
+public class test {
+    private static final long a = 24 * 60 * 60 * 1000 * 25;
+    private static final long b = 24 * 60 * 60 * 1000;
+
+    public static void main(String[] args) {
+        System.out.println(a / b);
+    }
+}
--- src/main/java/September_18/Cat.java
+++ src/main/java/September_18/Cat.java
@@ -18,5 +18,5 @@ public class Cat extends Animal{
     public static void main(String[] args) {
         Cat cat=new Cat();
     }
-    
+
 }
